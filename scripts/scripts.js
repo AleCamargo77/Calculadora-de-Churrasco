a11918bea7b2fe327dac485b167854578edbb3ec
alessandro camargo <acamargodias@terra.com.br>
2022-06-18 23:13:59 -0300

    css button, responsividade ok

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,22 +4,14 @@ function resultBtn() {
     const child = document.getElementById('childCalc').value;
 
 
-        let resultMan = (man * 0.5);
-        let resultWoman = (woman * 0.3);
-        let resultChild = (child * 0.2);
-        let resultTotal = (resultMan + resultWoman + resultChild);
-        document.getElementById('result').innerHTML = `Total é de ${resultTotal} Kg`;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
+    let resultMan = (man * 0.5);
+    let resultBovine = (man * 0.25);
+    let resultChicken = (man * 0.1);
+    let resultSausage = (man * 0.15);
+    
+    let resultWoman = (woman * 0.3);
+    let resultChild = (child * 0.2);
+    let resultTotal = (resultMan + resultWoman + resultChild);
+    document.getElementById('result').innerHTML = `Total é de ${resultTotal} Kg`;
+    document.getElementById('result').style.display = 'block';
+}
